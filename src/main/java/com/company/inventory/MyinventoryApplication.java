9dfe955b08fe6e4428b6a740a5bf14079561b041
pduracell513-collab <pduracell513@gmail.com>
2025-10-07 23:45:21 -0600

    proyects base

---
+++ src/main/java/com/company/inventory/MyinventoryApplication.java
@@ -0,0 +1,13 @@
+package com.company.inventory;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyinventoryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MyinventoryApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/company/inventory/MyinventoryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.company.inventory;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyinventoryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
