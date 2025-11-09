b891f242ac737144082be8bc2144752894e1bec9
pduracell513-collab <pduracell513@gmail.com>
2025-11-09 11:23:50 -0600

    Agregando actualizar categorias

--- src/main/java/com/company/inventory/services/CategoryServiceImpl.java
+++ src/main/java/com/company/inventory/services/CategoryServiceImpl.java
@@ -84,6 +84,43 @@ public class CategoryServiceImpl implements ICategoryService{
 		}
 		return new ResponseEntity<CategoryResponseRest>(response,HttpStatus.OK);
 	}
+	@Override
+	@Transactional
+	public ResponseEntity<CategoryResponseRest> update(Category category, Long id) {
+		CategoryResponseRest response= new CategoryResponseRest();
+		List<Category> list=new ArrayList<>();
+		try {
+			Optional<Category> categorySearch=categoryDao.findById(id);
+			if(categorySearch.isPresent()) {
+				//se actualiza el registro
+				categorySearch.get().setName(category.getName());
+				categorySearch.get().setDescription(category.getDescription());
+				Category categoryToUpdate=categoryDao.save(categorySearch.get());
+				
+				if(categoryToUpdate !=null) {
+					list.add(categoryToUpdate);
+					response.getCategoryResponse().setCategory(list);
+					response.setMetadata("RESPUESTA OK", "-1", "Categoria guardada");
+					
+				}else {
+		        	response.setMetadata("Respuesta NO ok", "-1", "Categoria no actualizada ");
+			        return new ResponseEntity<CategoryResponseRest>(response,HttpStatus.BAD_REQUEST);
+					
+				}
+				
+			}else {response.setMetadata("Respuesta NO ok", "-1", "Categoria no actualizada ");
+	        return new ResponseEntity<CategoryResponseRest>(response,HttpStatus.NOT_FOUND);
+				
+			}
+
+	}catch(Exception e) {
+			response.setMetadata("Respuesta NO ok", "-1", "Error al actualizar categoria ");
+	        e.getStackTrace();
+	        return new ResponseEntity<CategoryResponseRest>(response,HttpStatus.INTERNAL_SERVER_ERROR);
+	        
+		}
+		return new ResponseEntity<CategoryResponseRest>(response,HttpStatus.OK);
+	}
 
 }
 
