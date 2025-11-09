e6c7242139c9705f58881e3fa8a053479ed33efd
pduracell513-collab <pduracell513@gmail.com>
2025-11-09 05:13:30 -0600

    Agregando guardar categorias

--- src/main/java/com/company/inventory/services/CategoryServiceImpl.java
+++ src/main/java/com/company/inventory/services/CategoryServiceImpl.java
@@ -60,4 +60,30 @@ public class CategoryServiceImpl implements ICategoryService{
 		}
 		return new ResponseEntity<CategoryResponseRest>(response,HttpStatus.OK);
 	
-	}}
+	}
+	@Transactional
+	@Override
+	public ResponseEntity<CategoryResponseRest> save(Category category) {
+		CategoryResponseRest response= new CategoryResponseRest();
+		List<Category> list=new ArrayList<>();
+		try {
+        Category categorySaved=categoryDao.save(category);
+        if (categorySaved !=null) {
+        	list.add(categorySaved);
+        	response.getCategoryResponse().setCategory(list);
+        	response.setMetadata("Respuesta NO ok", "00", "CATEGORIA GUARDADA ");
+        }else {
+        	response.setMetadata("Respuesta NO ok", "-1", "Categoria no guardada ");
+	        return new ResponseEntity<CategoryResponseRest>(response,HttpStatus.BAD_REQUEST);
+        }
+	}catch(Exception e) {
+			response.setMetadata("Respuesta NO ok", "-1", "Error al guardar categoria ");
+	        e.getStackTrace();
+	        return new ResponseEntity<CategoryResponseRest>(response,HttpStatus.INTERNAL_SERVER_ERROR);
+	        
+		}
+		return new ResponseEntity<CategoryResponseRest>(response,HttpStatus.OK);
+	}
+
+}
+
