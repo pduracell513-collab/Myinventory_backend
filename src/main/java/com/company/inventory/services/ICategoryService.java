3531ecb5933d143b1c4837f368085ae90211b2df
pduracell513-collab <pduracell513@gmail.com>
2025-11-12 15:11:32 -0600

    Agregando eliminar categorias

--- src/main/java/com/company/inventory/services/ICategoryService.java
+++ src/main/java/com/company/inventory/services/ICategoryService.java
@@ -10,4 +10,5 @@ public ResponseEntity<CategoryResponseRest> search();
 public ResponseEntity<CategoryResponseRest> searchById(Long id);
 public ResponseEntity<CategoryResponseRest> save(Category category);
 public ResponseEntity<CategoryResponseRest> update(Category category,Long id);
+public ResponseEntity<CategoryResponseRest> deleteById(Long id);
 }
